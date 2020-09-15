1f33ffa45bcbcc8b4ac6374f55f67d2ba936963d
luislortega <a18016323@alumnos.uady.mx>
2020-09-15 17:00:11 -0500

    initial config

---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="panel-text-chat" id="container" style="max-height:200px; overflow-y: auto;">
+    </div>
+    <div>
+      <br />
+      <br />
+      <input type="text" name="username" placeholder="Escribe tu username" class="message" />
+      <br />
+      <br />
+      <input type="text" name="message" placeholder="Escribe tu mensaje aqui" class="message" />
+      <br />
+      <br />
+      <br />
+      <button class="btn_enviar">Enviar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted: function () {
+    this.scrollToEnd()
+  },
+  methods: {
+    scrollToEnd: function () {
+      var container = this.$el.querySelector("#container");
+      container.scrollTop = container.scrollHeight;
+    },
+  },
+};
+</script>
+<style scoped>
+.panel-text-chat {
+  padding: 2vw;
+  text-align: left;
+  height: 25vw;
+  background: #cccccc;
+  border-radius: 10px;
+  overflow: scroll;
+}
+.btn_enviar {
+  width: 30%;
+  height: 3vw;
+}
+.message {
+  width: 70%;
+  height: 3vw;
+}
+</style>
